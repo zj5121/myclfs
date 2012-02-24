a59d8f535e24a41b8c6e5591ccccf6a2f67e7d61
eric zhu <simonz5121@gmail.com>
2012-02-24 01:53:07 -0500

    change build root

--- mk/config.mk
+++ mk/config.mk
@@ -1,23 +1,23 @@
 # follow clfs
+curdir := $(shell pwd)
 
 TARGET := i686-unknown-linux-gnu
 TARGET_ARCH := i386
 
 NR_CPU := $(shell cat /proc/cpuinfo|grep processor|wc -l)
 MAKE := make -j$(NR_CPU)
-BASE := /cross
+BASE := $(curdir)/crossbuild
 TOOLS := /tools
 CROSS_TOOLS := /cross_tools
 PATH := $(CROSS_TOOLS)/bin:/bin:/usr/bin
 
 BUILD := $(shell gcc -dumpmachine)
 
-curdir := $(shell pwd)
 
 MYPATCHES_DIR := $(curdir)/mypatches
 PATCHES_DIR := $(curdir)/patches
 
-ROOT := $(shell pwd)
+ROOT := $(curdir)
 DOWNLOAD := $(ROOT)/download
 BLD := $(ROOT)/bld
 SRC := $(ROOT)/src
